8d991bcf8dff7dc4bc407d0bed23511e781d16d5
asur1357 <sukanil1357@gmail.com>
2025-06-29 13:45:41 +0530

    :bug: vercel error

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 import path from "path"
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(
+    {
+      tsDecorators: true,
+      sourcemap: false,
+    }
+  )],
   build: {
+    sourcemap: false,
+  },
+  esbuild: {
     sourcemap: false,
   },
   resolve: {
